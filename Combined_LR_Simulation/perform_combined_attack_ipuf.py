c20c1d1916d44bd29988ddcdb09a94e4c8843826
niloufarsyd <32245063+niloufarsyd@users.noreply.github.com>
2024-05-07 16:40:01 +0200

    Update perform_combined_attack_ipuf.py

--- Combined_LR_Simulation/perform_combined_attack_ipuf.py
+++ Combined_LR_Simulation/perform_combined_attack_ipuf.py
@@ -52,7 +52,9 @@ attack_parameters = {'num_pufs_to_attack': 1,
                      'add_validation': True,
                      'add_puf_to_fit': True,
                      'perform_reliability_attack': True,
-                     'perform_NFPr_attack':True,# 'perform_NFPr_attack':True for the non-flipping-probability based combined attach and 'perform_NFPr_attack':False for the reliability based combined attack
+                     'perform_NFPr_attack':True, 
+                      #when 'perform_NFPr_attack' is 'True', the Combined Chosen Challenge attack is done (which is based on non-flipping probability information),
+                      #and 'perform_NFPr_attack':'False', the Combined Reliability based attack is done. The 'True' value has the priority for this parameter.
                      'nSamples':16,
                      'num_flips_for_NFPr':1,
                      'reliability_repetitions': 17,# because nSamples is 16, to have a fair comparison the reliability_repetitions should be 16+1
